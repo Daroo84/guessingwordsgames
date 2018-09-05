acf56297eee282e01fc3fb1a28ee1beae82e552e
Daroo84 <darekstaron@gmail.com>
2018-09-05 13:57:41 +0200

    Second commit

--- src/Game.java
+++ src/Game.java
@@ -8,23 +8,29 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        int fails = 0;
+        int round = 0;
         String response;
-        while (true){
+        while (fails <= 3){
+            round++;
+
             String word = words[random.nextInt(words.length)];
             String hashWord = word;
 
             char randomChar = hashWord.charAt(random.nextInt(hashWord.length()));
             hashWord = hashWord.replaceFirst(String.valueOf(randomChar), "*");
-
             System.out.println("Twoje wylosowane slowo: " + hashWord);
             System.out.print("Podaj slowo: ");
             response = scanner.nextLine();
 
             if (word.equalsIgnoreCase(response)){
                 System.out.println("Congratulations");
+            }else{
+                System.out.println("NIe udalo sie odgadnac");
+                fails++;
             }
 
         }
-
+        System.out.println("Koniec gry! Twoje punkty: " + (round -fails));
     }
 }
